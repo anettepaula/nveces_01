ff9b0aebd4baac3bed6a719f4ceacaf79361f8ce
ism06659681 <ism06659681@a04.informatica.escoladeltreball.org>
2018-01-31 11:39:50 +0100

    Bucle

---
+++ mayork03.py
@@ -0,0 +1,5 @@
+#coding: utf-8
+num1 = input ("Escriba un número")
+num2 = input ("Escriba un número mayor")
+while (num1<num2)
+	num2 = input("introduce el siguiente número")
---
+++ nveces_01.py
@@ -0,0 +1,7 @@
+#coding: utf-8
+x = int(input("Introduce numero:"))
+i = int(0)
+while i < x :
+	print input("Introduce un número:")
+	i += 1
+
